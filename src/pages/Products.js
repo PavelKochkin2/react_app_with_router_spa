cf7017c08fac3fd7e1857cc33074d7570e8ea872
Pavel Kochkin <kach93@mail.ru>
2023-10-24 17:36:00 +0200

    added back btn

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {
+    id: "1",
+    name: "product 1",
+  },
+  { id: "2", name: "product 2" },
+  { id: "3", name: "product 3" },
+];
+
 export const ProductsPage = () => {
-  return <div>Welcome to Products Page</div>;
+  return (
+    <>
+      <ul>
+        {PRODUCTS.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <p>
+        {" "}
+        <Link to="..">Back</Link>
+      </p>{" "}
+    </>
+  );
 };
